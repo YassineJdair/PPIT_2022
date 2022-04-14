107cde3b919db9da32210073231f0a0586d1204c
keemo01 <78498562+keemo01@users.noreply.github.com>
2022-04-14 19:54:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Navbar } from "react-bootstrap";
+import { Card, Navbar } from "react-bootstrap";
 import { Nav } from "react-bootstrap";
 import {
   BrowserRouter as Router,
@@ -9,46 +9,55 @@ import {
   Switch,
 } from "react-router-dom";
 import { Exchanges } from "./Components/exchanges";
-import { Learn } from "./Components/learn";
+import Learn from "./Components/learn";
 import Currency from "./Components/currency";
 import { Add } from "./Components/add";
 import { View } from "./Components/view";
 import { Update } from "./Components/update";
 import { LogRegister } from "./Components/LogRegister";
 
-//https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false
-//"https://api.coinstats.app/public/v1/coins?skip=0"
+
+import logo from '../src/images/logo.jpg'
+import Nft from "./Components/Nft";
 
 class App extends Component {
   render() {
     return (
+
       <Router>
         <div className="App">
-          <Navbar bg="grey" variant="dark">
+          <Navbar className="w-full flex md:justify-center justify-between items-center p-4 color-nav">
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+              <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
             <Navbar.Brand href="/home">Cryptonia</Navbar.Brand>
             <Nav className="mr-auto">
               <Nav.Link href="/exchanges">Exchanges</Nav.Link>
-              <Nav.Link href="/learn">About Crypto</Nav.Link>
+              <Nav.Link href="/learn">Learn</Nav.Link>
               <Nav.Link href="/add">Add Crypto</Nav.Link>
               <Nav.Link href="/view">View portfolio</Nav.Link>
               <Nav.Link href="/LogRegister">Login/Register</Nav.Link>
+              <Nav.Link href="/nft">Nft Portfolio</Nav.Link>
+
             </Nav>
           </Navbar>
 
-          <br />
 
           <Route>
             <Route path="/" component={Currency} exact />
             <Route path="/home" component={Currency} exact />
             <Route path="/exchanges" component={Exchanges} exact />
-            <Route path="/learn" component={Learn} exact />
-            <Route path="/add" component={Add} />
+            <Route path="/learn" component={Learn} />
+            <Route path="/welcome" component={Add} />
             <Route path="/view" component={View} />
             {<Route path="/update" component={Update} />}
             <Route path="/LogRegister" component={LogRegister} />
+            <Route path="/nft" component={Nft} />
+
           </Route>
         </div>
       </Router>
+
     );
   }
 }
